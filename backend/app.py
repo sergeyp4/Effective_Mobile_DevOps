cbeab1372c349e0de6b8135ef8b9d2004b661e3d
Sergey Piontkovsky <sergeyp4@yandex.ru>
2026-04-06 21:50:01 +0300

    Web server improved

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,31 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+HOST = ""
+PORT = 8080
+
 class SimpleHandler(BaseHTTPRequestHandler):
+    def send_custom_response(self, status_code, body, content_type='text/html'):
+        self.send_response(status_code)
+        self.send_header('Content-type', f'{content_type}; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(body.encode('utf-8'))
+
     def do_GET(self):
         if self.path == '/':
-            response_code = 200
-            response_text = "Hello from Effective Mobile!"
+            self.send_custom_response(200, "Hello from Effective Mobile!")
         else:
-            response_code = 404
-            response_text = "Page not found"
-        self.send_response(response_code)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(response_text.encode())
+            self.send_custom_response(404, "<h1>404</h1><p>Page not found</p>")
 
-port = 8080
-server_address = ('', port)
-httpd = HTTPServer(server_address, SimpleHandler)
-
-print(f"Listening port {port}")
-httpd.serve_forever()
+def run_server():
+    web_server = HTTPServer((HOST, PORT), SimpleHandler)
+    print(f"Server started at http://{HOST}:{PORT}")
+    
+    try:
+        web_server.serve_forever()
+    except (KeyboardInterrupt, SystemExit):
+        print("\nStopping server...")
+    finally:
+        web_server.shutdown()
+    
+if __name__ == '__main__':
+    run_server()
